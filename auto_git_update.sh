e6bfa138c1c12ea82bc14226552bffb38fd91133
Filip Žitný <filip.zitny@deepnote.com>
2025-02-11 01:24:09 +0100

    initial backup

--- auto_git_update.sh
+++ auto_git_update.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-while true; do
-
-    # Add all changes
-    git add .
-
-    # Create commit with current date
-    git commit -m "update $(date '+%Y-%m-%d %H:%M:%S')"
-
-    # Push to main branch
-    git push origin main
-
-    # Wait for 24 hours (86400 seconds)
-    sleep 86400
-done
+git add .
+git commit -m "update $(date '+%Y-%m-%d %H:%M:%S')"
+git push origin main
